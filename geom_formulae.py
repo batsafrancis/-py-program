36dd4f0429352d495a912c3f3516fa51a84bdfde
francis <fbatsa@aims.edu.com>
2014-08-30 10:24:09 +0000

    the message!

---
+++ geom_formulae.py
@@ -0,0 +1,185 @@
+__author__ = 'francis'
+__author__ = 'francis'
+from numbers import Number
+from numpy import *
+
+def  triangular_area (height : Number ,length : Number)->Number:
+    """
+calculate the area of a triangle
+    @param heigth:The heigth of triangle
+    @param length:the length of the triangle
+    @return:
+    >>> triangular_area(3,4)
+    23
+    """
+    return 0.5*height*length
+
+print(triangular_area(3,4))
+
+
+def rectangle_perimeter (length:Number ,width:Number)->Number:
+    """
+calculates the perimeter of a rectangle with
+
+  >>> rectangle_perimeter(3,4)
+    @param length: The length of the rectangle
+    @param width: The width of the rectangle
+    @return: The perimeter of rectangle (unit)
+    56
+    """
+    return 2*length + 2*width
+print(rectangle_perimeter(3,4))
+
+
+
+def cylinder_volume(radius : Number  ,height : Number)->Number:
+    """
+calculates the volume of the cylinder with
+
+ >>> cylinder_volume(radius,height)
+    @param radius:The radius of the cylinder
+    @param height: The height of the cylinder
+    @return:  Volume of cylinder (cudic units)
+    """
+    return pi*radius**2* height
+print(cylinder_volume(3,4))
+
+
+
+def cylinder_surface_area(radius : Number ,height: Number)-> Number:
+     """
+ calculates the surface area of a cylinder
+
+>>> cylinder_surface_area(3,4)
+     @param radius: The radius of the cylinder
+     @param height: The height of the cylinder
+     @return: surface area of a cylinder
+ 12
+     """
+     return 2*pi*radius*height + 2*pi*radius**2
+print( cylinder_surface_area(3,4))
+
+
+
+
+def cone_volume(radius,height):
+    """
+ Calculates the volume of a cone with
+
+>>> cone_volume(3,4)
+    @param radius: The radius of the cone
+    @param height: The height of the cone
+    @return: Volume of cone
+    """
+    return 1/3*pi*radius**height
+print(cone_volume(3,4))
+
+
+
+def sphere_volume(radius : Number) -> Number:
+    """
+Calculates the volume of a cone
+
+>>> sphere_volume(3)
+    @param radius:Base radius of the cone
+    @return: Volume of cone (cubic unit)
+    """
+    return 4*(pi/3*radius)
+print(sphere_volume(3))
+
+
+
+
+def frustum_cone_volume(top_radius:  Number    ,base_radius : Number  ,height : Number) ->Number:
+    """
+Calculates the volume of frustum cone with
+
+>>> frustum_cone_volume(2,3,4)
+    @param top_radius: The radius of the top or the cut
+    @param base_radius: The base radius
+    @param heigth:The height of of the frustum cone
+    @return:
+    """
+    return 1/3*pi*height*(top_radius**2 + top_radius*base_radius + base_radius**2)
+print(frustum_cone_volume(2,3,4))
+
+
+
+
+def parallelogram_area(base: Number,height: Number) -> Number:
+    """
+    calculate parallelogram area
+
+    >>> parallelogram_area(3,4)
+    @param base:The base of the parallelogram
+    @param height:The height of the parallelogram
+    @return: Area of th of the parallelogram
+    """
+    return base*height
+print(parallelogram_area(3,4))
+
+
+
+
+def parallelogram_area(base,height):
+    """
+   calculates the Area of parallelogram with
+
+>>> parallelogram_area(3,4)
+    @param base:        The base length
+    @param height: The height of the parallelogram
+    :return:Area parallelogram (unit square)
+    """
+    return base*height
+print(parallelogram_area(3,5))
+
+
+
+
+def rhombus_area(long_diagonal,short_diagonal):
+    """
+ calculate the Area of a rhombus with
+
+>>>  rhombus_area(1,2)
+    @param long_diagonal: the longest diagonal
+    @param short_diagonal: the shortest diagonal
+    @return: area of rhombus (area unit)
+    2
+    """
+    return 0.5*long_diagonal*short_diagonal
+print(rectangle_perimeter(1,1))
+
+
+
+
+
+def tetrahedron_volume(base: Number)-> Number:
+    """
+    calculate tetrahedron volume.
+
+    @param base: this is the base length
+    @return: volume of a tetrahedron (cubic unit)
+
+    >>> tetrahedron_volume(3)
+
+    10
+    """
+    return (base**2)/6*sqrt(2)
+
+print(tetrahedron_volume(2))
+
+
+
+
+def regular_pyramid_volume(base: Number , height:Number) -> Number:
+    """
+    calculate the volume of a regular pyramid.
+    >>>  regular_pyramid_volume(3,4)
+    @param base: Base length of the pyramid
+    @param height: Height of the pyramid
+    :return:volume of regular pyramid(cube unit)
+    """
+    return  1/3*base*height
+print(regular_pyramid_volume(3,4))
+
+
