45bb98d61163fb42f112b3359b99ab970aafa0d1
francis <fbatsa@aims.edu.com>
2014-08-30 09:50:58 +0000

    the message!

--- BATSA.py
+++ BATSA.py
@@ -3,4 +3,4 @@ __author__ = 'francis'
 print('hellow world vkhjkiuo')
 print("hellow world")
 print("Ghana !,a beautiful country")
-print ("hi Carl ! wonderful class .nice  ")
+print ("hi Carl ! wonderful class .nice joijo ")
